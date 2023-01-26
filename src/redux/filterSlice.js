42d14674e701e98031b508bddef6498f35acf2f9
NadyaKonzevaya <borisova.nadya@gmail.com>
2023-01-26 14:49:15 +0300

    Update filterSlice.js

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
 // const persistConfig = {
 //   key: 'filter',
